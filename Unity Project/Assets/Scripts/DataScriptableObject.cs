77a678e9cf65d8031ef12c2c446824a86733bb7d
IceDarold <93391108+IceDarold@users.noreply.github.com>
2024-10-23 14:50:21 +0500

    Delete Unity Project directory

--- Unity Project/Assets/Scripts/DataScriptableObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Data", order = 1)]
-public class DataScriptableObject : ScriptableObject
-{
-    public int points;
-}
--- Unity Project/Assets/Scripts/Game/Cube.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Cube : MonoBehaviour
-{
-    void OnMouseDown()
-    {
-        FindObjectOfType<CubeGame>().OnCubeClicked(gameObject);
-    }
-}
