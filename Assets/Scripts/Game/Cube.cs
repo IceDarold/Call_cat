bf09dde210554bbc04fc12a0011780fdb5ed5f2d
Youggood <ymnik50s@gmail.com>
2024-10-25 19:38:37 +0300

    add particles

--- Assets/Scripts/Game/Cube.cs
+++ Assets/Scripts/Game/Cube.cs
@@ -1,9 +1,21 @@
+using System.Collections;
 using UnityEngine;
 
 public class Cube : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem particles;
     void OnMouseDown()
     {
+        particles.Play();
+        GetComponent<SpriteRenderer>().enabled = false;
+        StartCoroutine(Coroutine());
+    }
+
+
+    private IEnumerator Coroutine()
+    {
+        yield return new WaitForSeconds(1f);
         FindObjectOfType<CubeGame>().OnCubeClicked(gameObject);
+        yield break;
     }
 }
