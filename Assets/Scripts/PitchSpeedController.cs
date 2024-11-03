28e9ed0e1478deb5a23761c3b69aec73a1adccc0
Youggood <ymnik50s@gmail.com>
2024-11-03 22:01:49 +0300

    add decrease pitch button

--- Assets/Scripts/PitchSpeedController.cs
+++ Assets/Scripts/PitchSpeedController.cs
@@ -5,34 +5,60 @@ using UnityEngine.UI;
 
 public class PitchSpeedController : MonoBehaviour
 {
-    [SerializeField] private Button pitchButton;
+    [SerializeField] private Button pitchIncreaseButton;
+    [SerializeField] private Button pitchDecreaseButton;
+
     [SerializeField] private AudioSource audioSource;
-    [SerializeField] private float deltaPitchPerClick;
+    [SerializeField] private float deltaPitchIncreasePerClick;
+    [SerializeField] private float deltaPitchDecreasePerClick;
+
     [SerializeField] private float deltaPitchPerSecond;
 
 
     private void Awake()
     {
-        pitchButton.onClick.AddListener(PitchClick);
+        pitchIncreaseButton.onClick.AddListener(PitchClickIncrease);
+        pitchDecreaseButton.onClick.AddListener(PitchClickDecrease);
     }
 
     private void OnDestroy()
     {
-        pitchButton.onClick.RemoveListener(PitchClick);
-    }
+        pitchIncreaseButton.onClick.RemoveListener(PitchClickIncrease);
+        pitchDecreaseButton.onClick.RemoveListener(PitchClickDecrease);
+    } 
     private void Update()
     {
         if(audioSource.pitch > 1)
         {
             audioSource.pitch -= deltaPitchPerSecond * Time.deltaTime;
         }
+
+        if(audioSource.pitch < 1)
+        {
+            audioSource.pitch += deltaPitchPerSecond * Time.deltaTime;
+        }
+
+        if(Mathf.Abs(audioSource.pitch - 1) < 0.01f )
+        {
+            audioSource.pitch = 1;
+        }
     }
 
-    private void PitchClick()
+    private void PitchClickIncrease()
     {
-        if(audioSource.pitch < 3)
+        if(audioSource.pitch < 3 && audioSource.pitch >= 1)
         {
-            audioSource.pitch += deltaPitchPerClick;
+            audioSource.pitch += deltaPitchIncreasePerClick;
+        }
+    }
+
+    private void PitchClickDecrease()
+    {
+       
+        if(audioSource.pitch > -3 && audioSource.pitch <= 1)
+        {
+            Debug.Log("-");
+            audioSource.pitch -= deltaPitchDecreasePerClick;
         }
     }
 
